c5228904abf22a30cb24396661432c7e2c00d8da
Mayank Bhadana <mkbhdana@gmail.com>
2022-11-26 19:22:58 +0530

    Update Select.js

--- src/components/Table/Select.js
+++ src/components/Table/Select.js
@@ -77,6 +77,7 @@ export default function RowRadioButtonsGroup({ onChange }) {
                     color: blue[500],
                   },
                 },
+                display: "none",
               }}
             />
           }
